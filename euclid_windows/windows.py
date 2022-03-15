705b7091a3cb91fa2a1348fdc6f781fc79db496e
Luca Pagano <lu.pagano@gmail.com>
2022-03-15 10:20:15 +0100

    some reworking and bias added

--- euclid_windows/windows.py
+++ euclid_windows/windows.py
@@ -1,8 +1,7 @@
 import numpy as np
 from scipy import integrate
-import math
 
-class Win:
+class Windows:
     def __init__(self,
         zmax=2.5,
         nbin=3,
@@ -10,6 +9,7 @@ class Win:
         dz=0.001,
         sigma_ph=0.05,
         bintype="equipopulated",
+
     ):
 
         self.zmax=zmax
@@ -46,13 +46,13 @@ class Win:
         zeta = np.linspace(0, self.zmax, num=self.nz)
 
         eta_z = np.zeros((self.nz, self.nbin), "float64")
-        gal = galaxy_distribution(zeta, True)
+        gal = galaxy_distribution(zeta)
         for Bin in range(self.nbin):
             low = self.z_bin_edge[Bin]
             hig = self.z_bin_edge[Bin + 1]
             for inz in range(self.nz):
                 z = zeta[inz]
-                integrand = gal * photo_z_distribution(z, zeta, self.sigma_ph, True)
+                integrand = gal * photo_z_distribution(z, zeta, self.sigma_ph)
                 integrand = np.array(
                     [
                         elem if low <= zeta[index] <= hig else 0
@@ -73,44 +73,31 @@ class Win:
         for Bin in range(self.nbin):
             eta_z[:, Bin] /= eta_norm[Bin]
 
-        return zeta, eta_norm, eta_z
+        self.zeta = zeta
+        self.eta_norm = eta_norm
+        self.eta_z = eta_z
+        self.bias = np.sqrt(1+(self.z_bin_edge[1:]+self.z_bin_edge[:-1])/2)
+
+        return
 
 
-def galaxy_distribution(z, zmean = 0.9,  array=False):
+def galaxy_distribution(z, zmean = 0.9):
     """
     Galaxy distribution returns the function D(z) from the notes
-
-    If the array flag is set to True, z is then interpretated as an array,
-    and not as a single value.
 
     """
     z0 = zmean/np.sqrt(2.0)
 
-    if not array:
-        galaxy_dist = z**2*math.exp(-(z/z0)**(1.5))
-    else:
-        galaxy_dist = z**2*np.exp(-(z/z0)**(1.5))
+    galaxy_dist = z**2*np.exp(-(z/z0)**(1.5))
 
     return galaxy_dist
 
-def photo_z_distribution(z, zph, sigma_ph, array=True):
+def photo_z_distribution(z, zph, sigma_ph):
     """
     Photo z distribution
-    If the array flag is set to True, z is then interpretated as an array,
-    and not as a single value.
     """
-
-    # Note: you must normalize it yourself to one if you want to get nice
-    # plots of the galaxy distribution function in each bin (otherwise, the
-    # spectra will remain correct, but each D_i(x) will loot strangely
-    # normalized when compared to the original D(z)
-    if not array:
-        photo_z_dist = math.exp(-0.5*(
-            (z-zph)/sigma_ph/(1.+z))**2)/sigma_ph/(1.+z)/math.sqrt(
-            2.*math.pi)
-    else:
-        photo_z_dist = np.exp(-0.5*(
-            (z-zph)/sigma_ph/(1.+z))**2)/sigma_ph/(1.+z)/math.sqrt(
-            2.*math.pi)
+    photo_z_dist = np.exp(-0.5*(
+        (z-zph)/sigma_ph/(1.+z))**2)/sigma_ph/(1.+z)/np.sqrt(
+        2.*np.pi)
 
     return photo_z_dist
